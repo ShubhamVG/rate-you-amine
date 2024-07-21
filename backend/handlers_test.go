dad166996b6411b69e394e54ad191a7a15663bce
ShubhamVG <vgshubham49@gmail.com>
2024-07-21 22:40:38 +0530

    bad json test added
    also, sleep duration decreased from 1 second to 10 millisecond when starting the server

--- backend/handlers_test.go
+++ backend/handlers_test.go
@@ -16,7 +16,7 @@ func TestLoginHandler(t *testing.T) {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/v1/login", loginHandler).Methods("POST")
 	go http.ListenAndServe("localhost:8080", router)
-	time.Sleep(1 * time.Second)
+	time.Sleep(10 * time.Millisecond)
 
 	t.Run("email does not exist", func(t *testing.T) {
 		reqBody := []byte(`{"email": "dne@hell.com", "password": "ashdfiashkfljadsfds"}`)
@@ -150,6 +150,33 @@ func TestLoginHandler(t *testing.T) {
 
 		if string(respBody) != EMAIL_PASSWORD_MISSING_MSG {
 			t.Fatal("password was missing but ugh wtf happened?")
+		}
+	})
+
+	t.Run("bad json", func(t *testing.T) {
+		reqBody := []byte(`{"email": "test@gmail.com,}"`)
+		req, err := http.NewRequest("POST", "http://localhost:8080/api/v1/login", bytes.NewBuffer(reqBody))
+		req.Header.Set("Content-Type", "application/json; charset=utf-8")
+
+		if err != nil {
+			t.Fatal("Failed to create request")
+		}
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+
+		if err != nil {
+			t.Fatal("Client failed to make request", err)
+		}
+
+		respBody, err := io.ReadAll(resp.Body)
+
+		if err != nil {
+			t.Fatal("io failed to read response body")
+		}
+
+		if string(respBody) != BAD_JSON_MSG {
+			t.Fatal("bad json got parsed?!?!?!?")
 		}
 	})
 }
